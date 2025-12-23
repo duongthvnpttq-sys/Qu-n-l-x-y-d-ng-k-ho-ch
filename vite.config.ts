743a33d8729fb71b5905797be055d19cd1f240cb
duongthvnpttq-sys <duongth.vnpttq@gmail.com>
2025-12-23 13:33:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Đảm bảo đường dẫn tương đối để Vercel không bị lỗi 404 assets
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor-react': ['react', 'react-dom'],
-          'vendor-utils': ['xlsx', 'jspdf'],
-          'vendor-charts': ['recharts']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 2000
+  server: {
+    historyApiFallback: true,
   }
-});
+})
